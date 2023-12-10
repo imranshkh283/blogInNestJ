56b7ceeb0b2817ef12b5efeea38f27c249e9adb6
Shaikh Imran <skimran0306@gmail.com>
2023-12-11 01:57:47 +0530

    remove unused controller route from app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
-import { AdminModule } from './admin/admin.modules';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ReportsBackendModule } from './admin/backend/reports/reports.module';
-import { UserBackendModule } from './admin/backend/user/user.module';
 import { PostModule } from './post/post.module';
 import { LoggerInterceptor } from './logger.interceptor';
 
 @Module({
-  imports: [
-    UserModule,
-    AdminModule,
-    RouterModule.register([
-      { path: 'admin/backend/', module: UserBackendModule },
-      { path: 'admin/backend/', module: ReportsBackendModule },
-    ]),
-    PostModule,
-  ],
+  imports: [UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService, LoggerInterceptor],
 })
