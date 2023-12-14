90f6e3f902e69e73e38f13a091dd2844c5a16d5b
Shaikh Imran <skimran0306@gmail.com>
2023-12-15 01:46:37 +0530

    create validation dto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,8 +1,19 @@
 import { PickType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CommentDto {
+  @IsNotEmpty()
+  @IsString()
   content: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
   postId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
   userId: number;
 }
 
