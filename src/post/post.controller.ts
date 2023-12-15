8f5296144ee782dd6a97d99736e80cbc89431e43
Shaikh Imran <skimran0306@gmail.com>
2023-12-15 20:05:12 +0530

    post dto implement

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -6,6 +6,7 @@ import {
   HttpStatus,
   Param,
   Post,
+  ValidationPipe,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -16,7 +17,7 @@ export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post('createPost')
-  createPost(@Body() data: CreatePostDto) {
+  createPost(@Body(ValidationPipe) data: CreatePostDto) {
     return this.postService.create(data);
   }
 
