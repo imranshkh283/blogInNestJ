b3ef221e8f83090577c54346149c6eca93673932
Shaikh Imran <skimran0306@gmail.com>
2023-12-15 20:05:04 +0530

    post dto implement

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,9 +1,40 @@
+import {
+  IsDate,
+  IsEmail,
+  IsEmpty,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreatePostDto {
+  @IsEmpty()
   id?: number;
-  title: string;
-  content: string;
-  // authorId: number;
-  createdAt: Date;
-  updatedAt: Date;
+
+  @IsNotEmpty({ message: 'Author email is required' })
+  @IsString()
+  @IsEmail()
+  @MinLength(3)
+  @MaxLength(200)
   email: string;
+
+  @IsNotEmpty({ message: 'Title is required' })
+  @IsString()
+  @MinLength(10)
+  title: string;
+
+  @IsNotEmpty()
+  @MinLength(10)
+  @IsString({ message: 'Content must be a string' })
+  content: string;
+
+  @IsDate()
+  @IsOptional()
+  createdAt: Date;
+
+  @IsDate()
+  @IsOptional()
+  updatedAt: Date;
 }
